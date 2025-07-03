660ba7b92ed18d2c7009a0721d146c59df014cbb
KeganCherry <kegan.cherry2@gmail.com>
2025-07-02 19:44:36 -0500

    created climber & intake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,9 +9,39 @@ public class Constants {
     }
 
     public static final class SHOOTER {
-        public static final int TOP_ROLLER_MOTOR_ID = -1;
-        public static final int BOTTOM_ROLLER_MOTOR_ID = -1;
-        public static final double TARGET_VELOCITY = 0.5; // rps
+        public static final int TOP_ROLLER_MOTOR_ID = -1; // TODO: Tune
+        public static final int BOTTOM_ROLLER_MOTOR_ID = -1; // TODO: Tune
+        public static final double VELOCITY_P = 0; // TODO: Tune
+        public static final double VELOCITY_I = 0; // TODO: Tune
+        public static final double VELOCITY_D = 0; // TODO: Tune
+        public static final double TARGET_VELOCITY = 0.5; // TODO: Tune
+    }
+
+    public static final class INTAKE {
+        public static final int LEFT_PIVOT_MOTOR_ID = -1; // TODO: Tune
+        public static final int RIGHT_PIVOT_MOTOR_ID = -1; // TODO: Tune
+        public static final double PIVOT_INTAKE_ANGLE = 0.0; // TODO: Tune
+        public static final double PIVOT_AMP_ANGLE = 0.0; // TODO: Tune
+        public static final double PIVOT_STOW_ANGLE = 0.0; // TODO: Tune
+        public static final double PIVOT_P = 0; // TODO: Tune
+        public static final double PIVOT_I = 0; // TODO: Tune
+        public static final double PIVOT_D = 0; // TODO: Tune
+
+        public static final int ROLLER_MOTOR_ID = -1; // TODO: Tune
+        public static final double ROLLER_VELOCITY_P = 0; // TODO: Tune
+        public static final double ROLLER_VELOCITY_I = 0; // TODO: Tune
+        public static final double ROLLER_VELOCITY_D = 0; // TODO: Tune
+        public static final double INTAKE_ROLLER_VELOCITY = 0.0; // TODO: Tune
+        public static final double AMP_ROLLER_VELOCITY = 0.0; // TODO: Tune
+        public static final double FEED_ROLLER_VELOCITY = 0.0; // TODO: Tune
+    }
+
+    public static final class CLIMBER {
+        public static final int LEFT_MOTOR_ID = -1; // TODO: Tune
+        public static final int RIGHT_MOTOR_ID = -1; // TODO: Tune
+        public static final double CLIMBER_SPEED = 0.5; // TODO: Tune
+        public static final boolean LEFT_CLIMBER_BROKEN = true;
+        public static final boolean RIGHT_CLIMBER_BROKEN = false;
     }
 
 }
--- src/main/java/frc/robot/subsystems/Climbers.java
+++ src/main/java/frc/robot/subsystems/Climbers.java
@@ -14,4 +14,6 @@ public class Climbers extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  
 }
+
