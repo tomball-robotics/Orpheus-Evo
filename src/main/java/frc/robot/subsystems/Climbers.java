3a245ac3c000503450a160633e05876fb8dd84ef
Daniel Hoffmaster <daniel.m.hoffmaster@gmail.com>
2025-07-04 14:34:25 -0500

    Added Climber subsystem

--- src/main/java/frc/robot/subsystems/Climbers.java
+++ src/main/java/frc/robot/subsystems/Climbers.java
@@ -4,15 +4,46 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Climbers extends SubsystemBase {
   /** Creates a new Climbers. */
-  public Climbers() {}
+  TalonFX climberPullR;
+  TalonFX climberPullL;
+
+  DutyCycleOut dutyCycle = new DutyCycleOut(Constants.CLIMBER.CLIMBER_SPEED);
+
+
+  public Climbers() {
+    if(Constants.CLIMBER.RIGHT_CLIMBER_BROKEN){
+      climberPullR = new TalonFX(Constants.CLIMBER.RIGHT_MOTOR_ID);
+    }
+    if(Constants.CLIMBER.LEFT_CLIMBER_BROKEN){
+      climberPullL = new TalonFX(Constants.CLIMBER.LEFT_MOTOR_ID);
+    }
+  }
+
+  public void setClimberSpeed(double output){
+    dutyCycle.Output = output;
+    try{
+      climberPullR.setControl(dutyCycle);
+    }catch(Exception e){}
+    try{
+      climberPullL.setControl(dutyCycle);
+    }catch(Exception e){}
+  }
+
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Target Speed", dutyCycle.Output);
+    SmartDashboard.putBoolean("Right Climber Working", climberPullR != null);
+    SmartDashboard.putBoolean("Left Climber Working", climberPullL != null);
   }
   
 }
