df221f856223e97b9843036799511b9d38c281b8
KeganCherry <kegan.cherry2@gmail.com>
2025-07-05 20:16:08 -0500

    finalized climbers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -64,6 +64,15 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
+
+        // Climber up when you press bumper, and down when you press trigger.
+        climbers.setDefaultCommand(
+            climbers.setClimberSpeed(
+                (driverController.leftBumper().getAsBoolean() ? Constants.CLIMBER.CLIMBER_SPEED : 0.0) - driverController.getLeftTriggerAxis()/2,
+                (driverController.rightBumper().getAsBoolean() ? Constants.CLIMBER.CLIMBER_SPEED : 0.0) - driverController.getRightTriggerAxis()/2
+            )
+        );
+
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         swerve.setDefaultCommand(
@@ -87,8 +96,7 @@ public class RobotContainer {
             point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))
         ));
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
+
         driverController.back().and(driverController.y()).whileTrue(swerve.sysIdDynamic(Direction.kForward));
         driverController.back().and(driverController.x()).whileTrue(swerve.sysIdDynamic(Direction.kReverse));
         driverController.start().and(driverController.y()).whileTrue(swerve.sysIdQuasistatic(Direction.kForward));
--- src/main/java/frc/robot/subsystems/Climbers.java
+++ src/main/java/frc/robot/subsystems/Climbers.java
@@ -9,8 +9,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 public class Climbers extends SubsystemBase {
 
@@ -20,16 +23,50 @@ public class Climbers extends SubsystemBase {
   DutyCycleOut leftDutyCycle = new DutyCycleOut(0);
   DutyCycleOut rightDutyCycle = new DutyCycleOut(0);
 
+  private final double MAX_EXTENSION_ROTATIONS = 50.0; // TODO: Tune
+  private final double MIN_RETRACTION_ROTATIONS = 0.0;
+  
   public Climbers() {
     leftClimber = new TalonFX(Constants.CLIMBER.RIGHT_MOTOR_ID);
     rightClimber = new TalonFX(Constants.CLIMBER.LEFT_MOTOR_ID);
+    
+    configureClimber(leftClimber);
+    configureClimber(rightClimber);
+    
+    leftClimber.setPosition(0);
+    rightClimber.setPosition(0);
   }
 
-  public Command setLeftClimberSpeed(double output){
+  private void configureClimber(TalonFX climber) {
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    
+    config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: Tune
+    
+    config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = MAX_EXTENSION_ROTATIONS;
+    config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = MIN_RETRACTION_ROTATIONS;
+    
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; i++) {
+      status = climber.getConfigurator().apply(config);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK()) {
+      System.out.println("Could not apply climber configs, error code: " + status.toString());
+    }
+  }
+
+  public Command setClimberSpeed(double leftOutput, double rightOutput){
     return new Command () {
       @Override
       public void initialize() {
-        leftDutyCycle.Output = Constants.CLIMBER.LEFT_CLIMBER_BROKEN ? 0 : output;
+    }
+
+      @Override
+      public void execute() {
+        leftClimber.setControl(leftDutyCycle.withOutput(Constants.CLIMBER.LEFT_CLIMBER_BROKEN ? 0 : leftOutput));
+        rightClimber.setControl(rightDutyCycle.withOutput(Constants.CLIMBER.RIGHT_CLIMBER_BROKEN ? 0 : rightOutput));
       }
 
       @Override
@@ -39,18 +76,23 @@ public class Climbers extends SubsystemBase {
     };
   }
 
-  public Command setRightClimberSpeed(double output){
-    return new Command () {
-      @Override
-      public void initialize() {
-        rightDutyCycle.Output = Constants.CLIMBER.RIGHT_CLIMBER_BROKEN ? 0 : output;
-      }
+  // Utility methods for position checking
+  public double getLeftClimberPosition() {
+    return leftClimber.getPosition().getValueAsDouble();
+  }
 
-      @Override
-      public boolean isFinished() {
-        return true;
-      }
-    };
+  public double getRightClimberPosition() {
+    return rightClimber.getPosition().getValueAsDouble();
+  }
+
+  public boolean isLeftClimberAtLimit() {
+    double position = getLeftClimberPosition();
+    return position <= MIN_RETRACTION_ROTATIONS || position >= MAX_EXTENSION_ROTATIONS;
+  }
+
+  public boolean isRightClimberAtLimit() {
+    double position = getRightClimberPosition();
+    return position <= MIN_RETRACTION_ROTATIONS || position >= MAX_EXTENSION_ROTATIONS;
   }
 
   @Override
@@ -59,19 +101,11 @@ public class Climbers extends SubsystemBase {
     SmartDashboard.putBoolean("Right Climber Broken", Constants.CLIMBER.RIGHT_CLIMBER_BROKEN);
     SmartDashboard.putNumber("Left Climber Speed", leftDutyCycle.Output);
     SmartDashboard.putNumber("Right Climber Speed", rightDutyCycle.Output);
+    
+    SmartDashboard.putNumber("Left Climber Position", getLeftClimberPosition());
+    SmartDashboard.putNumber("Right Climber Position", getRightClimberPosition());
+    SmartDashboard.putBoolean("Left Climber At Limit", isLeftClimberAtLimit());
+    SmartDashboard.putBoolean("Right Climber At Limit", isRightClimberAtLimit());
   }
   
 }
-
-/* FOR DANIEL
- * 
- * This subsystem will house the climber motors and their controls.
- * It will also handle the logic for climbing, such as checking if the climbers are broken,
- * and controlling the speed of the climbers.
- * 
- * You will probably need to use a dutycycleout to control the speed of the climbers,
- * so that they can be directly linked to an axis on the controller.
- * dutycycleout is where you feed it a value between -1 and 1,
- * and it will output that value to the motor, 
- * -1 being full reverse, 0 being stopped, and 1 being full forward.
- */
